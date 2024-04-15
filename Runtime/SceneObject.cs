33edd6eb718cad1833d85abd502e941b14ad1829
Xisco <xisco@xalocstudios.com>
2024-04-15 13:05:34 +0200

    make late update virtual

--- Runtime/SceneObject.cs
+++ Runtime/SceneObject.cs
@@ -33,7 +33,7 @@ namespace Xesin.GameplayFramework
             }
         }
 
-        private void LateUpdate()
+        protected virtual void LateUpdate()
         {
             if (useControlRotation)
             {
